30d5dc466c0ddb9aeee2220b7c0c93a14c2b324c
michail <mail.mihail@protonmail.com>
2022-08-03 12:36:13 +0300

    add basic validation

--- BE/graphql/resolvers.js
+++ BE/graphql/resolvers.js
@@ -1,21 +1,37 @@
 const bcrypt = require("bcryptjs")
+const validator = require("validator")
+
 const User = require("../models/user")
 
 module.exports = {
   createUser: async function ({ userInput }) {
+    const errors = []
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: "email is invalid" })
+    }
+    if (
+      validator.isEmpty(userInput.password) ||
+      !validator.isLength(userInput.password, { min: 2 })
+    ) {
+      errors.push({ message: "password too short" })
+    }
+    if (errors.length) {
+      const error = new Error("Invalid input.")
+      throw error
+    }
     const existingUser = await User.findOne({ email: userInput.email })
-    
+
     if (existingUser) {
       const error = new Error("User exists already")
       throw error
     }
 
-        const hashedPass = await bcrypt.hash(userInput.password, 12)
-        const user = new User({
-          email: userInput.email,
-          name: userInput.name,
-          password: hashedPass,
-        })
+    const hashedPass = await bcrypt.hash(userInput.password, 12)
+    const user = new User({
+      email: userInput.email,
+      name: userInput.name,
+      password: hashedPass,
+    })
 
     const createdUser = await user.save()
     // use ._doc to get rid of mongoose metadata and ._od to transform from ObjectId => String
